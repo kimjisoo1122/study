0deb79b341c75cbf7fae123abc1596d02868bc41
kimjisoo1122 <kimjisoo4953@gmail.com>
2023-02-11 14:28:59 +0900

    Spring BeanPostProcessor

--- inflearn/helloboot/src/main/java/tobyspring/config/autoconfig/TomcatServerFactoryConfig.java
+++ inflearn/helloboot/src/main/java/tobyspring/config/autoconfig/TomcatServerFactoryConfig.java
@@ -4,16 +4,23 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 import tobyspring.config.ConditionalMyOnClass;
+import tobyspring.config.EnableMyConfigurationProperties;
 import tobyspring.config.MyAutoConfiguration;
 
 @MyAutoConfiguration
 @ConditionalMyOnClass("org.apache.catalina.startup.Tomcat")
+@Import(ServerProperties.class)
+@EnableMyConfigurationProperties(ServerProperties.class)
 public class TomcatServerFactoryConfig {
 
     @Bean("tomcatWebServerFactory")
     @ConditionalOnMissingBean
-    public ServletWebServerFactory servletWebServerFactory() {
-        return new TomcatServletWebServerFactory();
+    public ServletWebServerFactory servletWebServerFactory(ServerProperties serverProperties) {
+        TomcatServletWebServerFactory webServerFactory = new TomcatServletWebServerFactory();
+        webServerFactory.setContextPath(serverProperties.getContextPath());
+        webServerFactory.setPort(serverProperties.getPort());
+        return webServerFactory;
     }
 }
--- inflearn/helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ inflearn/helloboot/src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,10 +1,12 @@
 package tobyspring.helloboot;
 
+import org.springframework.boot.SpringApplication;
 import tobyspring.config.MySpringBootApplication;
 
 @MySpringBootApplication
 public class HellobootApplication {
+
     public static void main(String[] args) {
-        MySpringBootApplicationOld.run(HellobootApplication.class, args);
+        SpringApplication.run(HellobootApplication.class, args);
     }
 }
