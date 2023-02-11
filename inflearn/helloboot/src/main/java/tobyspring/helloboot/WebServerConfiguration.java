efbda2290bee80c7d59154f07d23d7595347efed
kimjisoo1122 <kimjisoo4953@gmail.com>
2023-02-11 18:55:37 +0900

    Property

---
+++ inflearn/helloboot/src/main/java/tobyspring/helloboot/WebServerConfiguration.java
@@ -0,0 +1,14 @@
+package tobyspring.helloboot;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebServerConfiguration {
+
+//    @Bean
+//    public ServletWebServerFactory customServletWebServerFactory() {
+//        TomcatServletWebServerFactory tomcatServletWebServerFactory = new TomcatServletWebServerFactory();
+//        tomcatServletWebServerFactory.setPort(9090);
+//        return tomcatServletWebServerFactory;
+//    }
+}
