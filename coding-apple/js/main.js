1bc17a20cee30e5ca42356089f7c4677fdc7153b
kimjisoo1122 <kimjisoo4953@gmail.com>
2023-02-22 00:07:21 +0900

    js

--- coding-apple/js/main.js
+++ coding-apple/js/main.js
@@ -1,7 +1,41 @@
-function pay(price, firstOrder) {
-	return firstOrder ? price * 0.9 - 1.5 : price * 0.9;
+// function pay(price, firstOrder) {
+// 	return firstOrder ? price * 0.9 - 1.5 : price * 0.9;
+// }
+//
+//
+// let pay1 = pay(10, true);
+// console.log(parseFloat(pay1.toFixed(2)));
+var 출석부 = ['흥민', '영희', '철수', '재석'];
+let string = 이름찾기('흥민1');
+console.log(string)
+function 이름찾기(name){
+	return 출석부.filter(s => name === s).length >= 1 ? '있어요' : '';
 }
 
 
-let pay1 = pay(10, true);
-console.log(parseFloat(pay1.toFixed(2)));
+function 구구단() {
+	for (let i = 2; i <= 9; i++) {
+		for (let k = 1; k <= 9; k++) {
+			console.log(i * k);
+		}
+	}
+}
+
+// 구구단();
+
+
+함수([40,40,40], 60);
+
+function 함수(arr, num) {
+	let sum = 0;
+	arr.forEach(i => {
+		sum += i;
+	})
+	let average = sum / Math.round(arr.length);
+	let result = num - average;
+	if (result > 0) {
+		console.log(`평균보다 ${result}점이 올랐네요`);
+	} else {
+		console.log(`평균보다 ${-result}점이 떨어졌네요 재수추천`);
+	}
+}
--- coding-apple/js/tab.js
+++ coding-apple/js/tab.js
@@ -1,6 +1,35 @@
-document.querySelector('.tab-button')
-		.addEventListener('click', () => {
-			document.querySelector('.tab-button').classList = 'tab-button orange';
-			document.querySelectorAll('.tab-button')[1].classList = 'tab-button';
-			
-		})
+// let tabBtns = document.querySelectorAll('.tab-button');
+// for (let i = 0; i < tabBtns.length; i++) {
+// 	tabBtns[i].addEventListener('click', () => {
+// 		let children = document.querySelector('.list').children;
+// 		for (const child of children) {
+// 			child.classList.remove('orange')
+// 		}
+// 		tabBtns[i].classList.add('orange');
+//
+// 		let tabContents = document.querySelectorAll('.tab-content');
+// 		for (const tabContent of tabContents) {
+// 			tabContent.classList.remove('show');
+// 		}
+// 		tabContents[i].classList.add('show');
+// 	})
+// }
+let tabBtns = $('.tab-button');
+let tabContents = $('.tab-content');
+
+function tapOpen(i) {
+	tabBtns.removeClass('orange');
+	tabBtns.eq(i).addClass('orange');
+	tabContents.removeClass('show');
+	tabContents.eq(i).addClass('show');
+}
+
+// for (let i = 0; i < tabBtns.length; i++) {
+// 	tabBtns.eq(i).on('click', () => {
+// 		tapOpen(i);
+// 	})
+// }
+
+$('.list').click(function (e) {
+	tapOpen(parseInt(e.target.dataset.id));
+})
